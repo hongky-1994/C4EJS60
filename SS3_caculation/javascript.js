17777ef995bda7d1761663c837f5b1c15d3615e7
hongky-1994 <hongkyhvnh@gmail.com>
2019-11-20 22:54:16 +0700

    creat SS3

---
+++ SS3_caculation/javascript.js
@@ -0,0 +1,11 @@
+//Nhập bán kính hình tròn
+let r = prompt("Hãy nhập bán kính hình tròn");
+
+
+// Tinh chu vi
+let chuvi = 2 * Math.PI * r;
+console.log("chu vi hinh tron la",chuvi);
+
+// Tinh dien tich
+let dientich = 4 * Math.PI * (r**2);
+console.log("dien tich hinh tron la ",dientich);
