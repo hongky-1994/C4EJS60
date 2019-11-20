91cf06c1751a541b679db507eed50c89e1e3b53e
hongky-1994 <hongkyhvnh@gmail.com>
2019-11-20 21:39:02 +0700

    homeword ss1

---
+++ SS2_var/var_intro.js
@@ -0,0 +1,14 @@
+
+// Hỏi tên
+let name = prompt("Hãy nhập tên bạn");
+
+// Hỏi tuổi và in ra console
+var age = prompt("Ban bao nhiêu tuổi");
+console.log("Xin chào ", name, " năm nay ", age," tuổi");
+
+
+// Hỏi năm sinh và in ra console
+let birthyear = prompt ("Bạn sinh năm bao nhiêu?");
+console.log("Xin chào bạn ",name," sinh năm ", birthyear);
+
+
