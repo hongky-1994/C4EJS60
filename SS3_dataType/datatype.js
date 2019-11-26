caba7c4bebffc35b4f47f93d11de3989124fbd49
hongky-1994 <hongkyhvnh@gmail.com>
2019-11-26 21:22:25 +0700

    SS4_condition

--- SS3_dataType/datatype.js
+++ SS3_dataType/datatype.js
@@ -1,6 +1,6 @@
-let number = 200;
-let String = "asfkanfkasnk";
-let school =`DH HN ${number}`;
+// let number = 200;
+// let String = "asfkanfkasnk";
+// let school =`DH HN ${number}`;
 
 // console.log(school);
 // console.log(typeof number);
@@ -9,12 +9,30 @@ let school =`DH HN ${number}`;
 
 // Thử với các phép toán tử
 
-let x = 50;
-let y = '30';
+// let x = 50;
+// let y = '30';
+// y = parseInt(y); // Đã chuyển biến String y về Number y;
+// let tong = `${x} + ${y} = ${x+y}`; console.log("Phép cộng",tong);
+// let hieu = `${x} - ${y} = ${x-y}`; console.log("Phép trừ là ",hieu);
+// let tich = `${x} * ${y} = ${x*y}`; console.log("Phép nhân là ",tich);
+// let thuong = `${x} / ${y} = ${x/y}`; console.log("Phép chia là ",thuong);
+// let soDu = `${x} % ${y} = ${x%y}`; console.log("Số dư là ",soDu);
 
-let tong = `${x} + ${y} = ${x+y}`; console.log("Phép cộng",tong);
-let hieu = `${x} - ${y} = ${x-y}`; console.log("Phép trừ là ",hieu);
-let tich = `${x} * ${y} = ${x*y}`; console.log("Phép nhân là ",tich);
-let thuong = `${x} / ${y} = ${x/y}`; console.log("Phép chia là ",thuong);
-let soDu = `${x} % ${y} = ${x%y}`; console.log("Số dư là ",soDu);
+// Math.pow(2,5); //Tương đương 2 mũ 5
+// Math.sin(30); // sin(30)
+// Math.sin(Math.PI); // sin(PI)
+// Math.sqrt(25); // Căn của 25
 
+// console.log(x); //50
+// console.log(x++); //50
+// console.log(++x); //51
+// console.log(--x); //51
+
+// BÀI TẬP TRONG GIỜ
+let a = Number(prompt("Nhập a đê :"));
+let b = Number(prompt("Nhap b de :"));
+
+console.log(`${a} + ${b} = ${a + b}`);
+console.log(`${a} - ${b} = ${a - b}`);
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
