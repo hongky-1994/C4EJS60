ae3fbb3084a729722292ec4b32e99bb51a38da1b
hongky-1994 <hongkyhvnh@gmail.com>
2019-11-26 20:26:54 +0700

    SS3

---
+++ SS3_dataType/datatype.js
@@ -0,0 +1,20 @@
+let number = 200;
+let String = "asfkanfkasnk";
+let school =`DH HN ${number}`;
+
+// console.log(school);
+// console.log(typeof number);
+// console.log(typeof String);
+// console.log(typeof school);
+
+// Thử với các phép toán tử
+
+let x = 50;
+let y = '30';
+
+let tong = `${x} + ${y} = ${x+y}`; console.log("Phép cộng",tong);
+let hieu = `${x} - ${y} = ${x-y}`; console.log("Phép trừ là ",hieu);
+let tich = `${x} * ${y} = ${x*y}`; console.log("Phép nhân là ",tich);
+let thuong = `${x} / ${y} = ${x/y}`; console.log("Phép chia là ",thuong);
+let soDu = `${x} % ${y} = ${x%y}`; console.log("Số dư là ",soDu);
+
