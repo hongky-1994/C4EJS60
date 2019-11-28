86678911e010e53ca42f09fbfccef9d66385b960
hongky-1994 <hongkyhvnh@gmail.com>
2019-11-28 13:03:23 +0700

    SS4 Home word

--- SS4_Condition/condition.js
+++ SS4_Condition/condition.js
@@ -20,13 +20,42 @@
 // }
 
 // cho người dùng nhập vào 1 tháng và in ra màn hình số ngày của tháng đó
-let x = Number(prompt("Nhập vào 1 tháng"));
-if(x === 1 || x===3 || x===5 || x===7 || x===8 || x===10 || x===12 ) {
-    console.log("31 ngày");
-} else if (x === 2) {
-    console.log("28 hoặc 29 ngày");
-} else if (x<=0 || x>12 || x != parseInt(x)) {
-    console.log("Nhập sai tháng");
-} else {
-    console.log("30 ngày");
+// let x = Number(prompt("Nhập vào 1 tháng"));
+// if(x === 1 || x===3 || x===5 || x===7 || x===8 || x===10 || x===12 ) {
+//     console.log("31 ngày");
+// } else if (x === 2) {
+//     console.log("28 hoặc 29 ngày");
+// } else if (x<=0 || x>12 || x != parseInt(x)) {
+//     console.log("Nhập sai tháng");
+// } else {
+//     console.log("30 ngày");
+// }
+
+// HOME WORK
+
+// Nhập số liệu
+let height = Number(prompt("Hãy nhập chiều cao của bạn (đơn vị cm)"));
+let weight = Number(prompt("Hãy nhập cân nặng của bạn (đơn vị kg)"));
+let BMI =  weight/(Math.pow(height/100,2));
+let body;
+// Điều kiện theo chỉ số tính BMI
+if( BMI < 18.5 && BMI >=0 ){
+    body = "Cân nặng thấp (Gầy)";
+} else if ( BMI >= 18.5 && BMI <25 ) {
+    body = "Bình thường";
+} else if ( BMI >= 25 && BMI <30 ) {
+    body = "Tiền béo phì";
+} else if ( BMI >= 30 && BMI <35 ) {
+    body = "Béo phì cấp độ I";
+} else if ( BMI >= 35 && BMI <40 ) {
+    body = "Béo phì cấp độ II";
+} else if ( BMI >= 40) {
+    body = "Béo phì cấp độ III";
+} else { 
+    body = "Bạn nhập sai dữ liệu"; 
 }
+ 
+// Hiển thị tình trạng của cơ thể 
+console.log("Chỉ số BMI theo số đo của bạn theo WHO là:", BMI);
+console.log("Tình trạng cơ thể bạn là:", body);
+
